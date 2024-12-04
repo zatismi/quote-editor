7804c51800ce57b7cfde2aeee82578da45dc6a8a
zatismi <zatismi@gmail.com>
2024-12-04 10:38:04 +0100

    Progress Bar Styling added and working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  # To test the Progress Bar style, we use the next line for delay that let us see it
+  # before_action -> { sleep 3 }
 end
