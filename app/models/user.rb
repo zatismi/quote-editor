74b3aee3786fafd18fd8d8b04b4ea9edc63e806e
zatismi <zatismi@gmail.com>
2024-12-17 11:57:17 +0100

    Playing around with Styling a little and the SignUp can now assign a company of the people but logic needs to be adjusted to look in the DB and check

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_validation :ensure_company, if: -> { company.blank? }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   belongs_to :company
+
+  def ensure_company
+    self.company ||= Company.create(name: "Company of the People")
+    debugger
+  end
 
   def name
     email.split("@").first.capitalize
