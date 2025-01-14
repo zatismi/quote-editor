f9755d7ba0bfc55b57a7a9e727a44ed5030f14ce
zatismi <zatismi@gmail.com>
2025-01-14 14:00:21 +0100

    Feat: Refactor text rotation in HelloUser controller to implement typing effect and improve user greeting experience

--- app/javascript/controllers/hello_user_controller.js
+++ app/javascript/controllers/hello_user_controller.js
@@ -12,37 +12,70 @@ export default class extends Controller {
   }
 
   // Default implementation without getting data from backend sent to here in the frontend
-  startTextRotation() {
-    this.interval = setInterval(() => {
-      this.index = (this.index + 1) % this.messages.length;
-      // A Very Good Use of Modulo Operator:
-      // a very nice way to switch between 0 and 1
-      // so according to the index starting number, it will switch to number -1 index of the array or the number of elements in the array
-      // so to switch between 4 elements the starting index should be 4, so the switch will start with 0, then 1, 2, 3 and then 0 again″
-      this.messageTarget.textContent = this.messages[this.index];
-    }, 2000);
-  }
+  // startTextRotation() {
+  //   this.interval = setInterval(() => {
+  //     this.index = (this.index + 1) % this.messages.length;
+  //     // A Very Good Use of Modulo Operator:
+  //     // a very nice way to switch between 0 and 1
+  //     // so according to the index starting number, it will switch to number -1 index of the array or the number of elements in the array
+  //     // so to switch between 4 elements the starting index should be 4, so the switch will start with 0, then 1, 2, 3 and then 0 again″
+  //     this.messageTarget.textContent = this.messages[this.index];
+  //   }, 2000);
+  // }
 
   // Implementation with getting data from backend sent to here in the frontend
-  startTextRotation() {
-    // Get the user's name from the data attribute
-    const userName = this.element.querySelector('[data-user-name]').getAttribute('data-user-name');
+  // startTextRotation() {
+  //   // Get the user's name from the data attribute
+  //   const userName = this.element.querySelector('[data-user-name]').getAttribute('data-user-name');
 
+  //   let texts = [
+  //     `Hello ${userName}`,
+  //     "I wish you a very nice day!"
+  //   ];
+
+  //   let index = 0;
+  //   this.messageTarget.textContent = texts[index];
+
+  //   setInterval(() => {
+  //     index = (index + 1) % texts.length;
+  //     this.messageTarget.textContent = texts[index];
+  //   }, 2000);
+  // }
+
+  // disconnect() {
+  //   clearInterval(this.interval); // Stop interval when disconnected
+  // }
+
+  startTextRotation() {
+    const userName = this.element.querySelector('[data-user-name]').getAttribute('data-user-name');
     let texts = [
       `Hello ${userName}`,
       "I wish you a very nice day!"
     ];
 
     let index = 0;
-    this.messageTarget.textContent = texts[index];
+    let charIndex = 0;
+    let currentText = texts[index];
+    let isDeleting = false;
 
-    setInterval(() => {
-      index = (index + 1) % texts.length;
-      this.messageTarget.textContent = texts[index];
-    }, 2000);
-  }
+    const type = () => {
+      this.messageTarget.textContent = currentText.substring(0, charIndex);
 
-  disconnect() {
-    clearInterval(this.interval); // Stop interval when disconnected
+      if (!isDeleting && charIndex < currentText.length) {
+        charIndex++;
+      } else if (isDeleting && charIndex > 0) {
+        charIndex--;
+      } else {
+        isDeleting = !isDeleting;
+        if (!isDeleting) {
+          index = (index + 1) % texts.length;
+          currentText = texts[index];
+        }
+      }
+
+      setTimeout(type, isDeleting ? 100 : 200);
+    };
+
+    type();
   }
 }
