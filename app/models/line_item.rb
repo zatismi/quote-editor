b5684da906569e1b33f187249009eac2e68be2be
zatismi <zatismi@gmail.com>
2025-01-15 15:01:15 +0100

    Feat: Setting few things for the theming

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,7 +2,7 @@ class LineItem < ApplicationRecord
   belongs_to :line_item_date
 
   validates :name, presence: true
-  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: -1 }
   validates :unit_price, presence: true, numericality: { greater_than: 0 }
 
   delegate :quote, to: :line_item_date
